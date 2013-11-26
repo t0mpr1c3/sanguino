b1e479042e2e420c96e792808a14adc81dc44754
Tom Price <t0mpr1c3@gmail.com>
2013-11-26 01:59:21 -0500

    sanguino_3x for Seeedstudio

--- hardware/merge-gerbers/merge-gerbers.sh
+++ hardware/merge-gerbers/merge-gerbers.sh
@@ -1,3 +1,4 @@
+cp ../gerbers/* .
 rm *.s#?
 rm *.b#?
 perl /Users/thomasprice/Documents/eagle/GerberMerger/GerberMerger.pl sanguino_3x_outline sanguino -1030 -1200
@@ -12,3 +13,5 @@ cp sanguino_3x_outline.GML sanguino_3x.GML
 rm sanguino_3x.zip
 tar -czf sanguino_3x.zip sanguino_3x.*
 rm sanguino_3x_outline_sanguino*
+rm sanguino.*
+rm sanguino_3x_outline.*
